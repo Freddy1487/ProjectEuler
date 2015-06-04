7147ed8a3836b92cfe01dee11623b3c1391101e3
Freddy1487 <freddy_krjuger@inbox.ru>
2015-06-04 11:46:11 +0300

    s

---
+++ Problem 12.java
@@ -0,0 +1,43 @@
+/* Problem 4 
+
+A palindromic number reads the same both ways. 
+The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+Find the largest palindrome made from the product of two 3-digit numbers.
+
+*/
+
+public class Problem4 {
+
+  
+    public static void main(String[] args) {
+       int max=0;
+       int n=0;
+        
+        
+        for(int i=100;i<999;i++)
+           for(int j=100;j<999;j++)
+           {
+               n=i*j;
+               if(isPolindromic(n))
+                   if(max<n)
+                       max=n;
+           }
+        
+        System.out.println(max);
+    }
+    
+    public static boolean isPolindromic(int n)
+    {
+        String str1,str2;
+        StringBuilder db1 =new StringBuilder(Integer.toString(n));
+        StringBuilder db2 =new StringBuilder(Integer.toString(n)).reverse();
+       
+        if(db1.toString().equals(db2.toString()))
+        {
+            return true;
+        }
+        return false;
+    }
+}
+ 
---
+++ Problem 8.java
@@ -0,0 +1,31 @@
+//projectEuler Problem 1
+
+/*
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+The sum of these multiples is 23.
+Find the sum of all the multiples of 3 or 5 below 1000.
+*/
+
+
+public class ProjectEuler
+{
+    public static void main(String[] args) {
+         P1 p=new P1();
+         System.out.println(p.getSum());
+    }
+}
+
+
+class P1
+{
+    public int getSum()
+    {
+        int sum=0;
+        for(int i=1;i<1000;i++)
+        {
+            if(i%3==0 || i%5==0)
+                sum+=i;
+        }
+        return sum;
+    }
+}
