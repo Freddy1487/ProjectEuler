a95d89a6f16bd5364bc8a34281c8a12ca3dd4ca5
Freddy1487 <freddy_krjuger@inbox.ru>
2015-06-08 15:18:08 +0300

    13
    solved

--- Problem 13.java
+++ Problem 13.java
@@ -1,6 +1,32 @@
-/* Problem 3  Largest prime factor
-The prime factors of 13195 are 5, 7, 13 and 29.
+/* 
+Problem 13
 
-What is the largest prime factor of the number 600851475143 ?
-
+Work out the first ten digits of the sum of the following one-hundred 50-digit numbers.
 */
+
+
+
+import java.io.*;
+import java.math.BigInteger;
+import java.util.ArrayList;
+
+ 
+public class P13 {
+
+     
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+         ArrayList<String> arr=readNumbers();
+         BigInteger bint=new BigInteger("0");
+         for(int i=0;i<arr.size();i++){
+             bint=bint.add(new BigInteger(arr.get(i)));
+         }
+         System.out.println(new String(bint.toString()).substring(0,10));
+    }
+    public static ArrayList readNumbers() throws FileNotFoundException, IOException{
+        BufferedReader rd=new BufferedReader(new FileReader("C:\\Users\\Freddy\\Documents\\NetBeansProjects\\P13\\src\\p13\\numbers.txt"));
+        ArrayList<String> arr=new ArrayList<>();
+        for(int i=0; rd.ready();i++)
+            arr.add(rd.readLine());
+        return arr;
+    }
+}
